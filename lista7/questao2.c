3f543657638ac6e8bb53db28c668d997a23c16ad
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2019-03-21 19:58:44 -0300

    Update questao2.c

--- lista7/questao2.c
+++ lista7/questao2.c
@@ -1,19 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* 
+/*
 calcular e escrever os n primeiros termos da sequencia (100-3c)/(c+1)
-
 1/100 + 97/2 + 3/94 + 91/4 + 5/88...
-
 impares = 1/100 + 3/94 + 5/88
 pares = 97/2 + 91/4 + 82/7
-
 */
 
 
 int main() {
-    
+
     int precisao =0;
     float resposta = 0;
     float c =0;
@@ -29,13 +26,16 @@ int main() {
     precisao -=2;
 
     num_de_termos_duplicados = precisao;
-       
-  
+
+
     for (precisao; precisao >= 0; precisao = precisao-2){
-        
+
         soma_impares = (( 2.0*c + parte_baixo) / (parte_cima-c*6.0));
- 
-        soma_pares = ((parte_cima-3.0 -6.0*c)/(2.0*c + 1.0 + parte_baixo));        
+
+        soma_pares = ((parte_cima-3.0 -6.0*c)/(2.0*c + 1.0 + parte_baixo));
+
+        printf("\n%.3f/%.3f", 2.0*c + parte_baixo,parte_cima-c*6.0 );
+        printf("\n%.3f/%.3f",parte_cima-3.0 -6.0*c, 2.0*c + 1.0 + parte_baixo );
 
 
         resposta = resposta + soma_impares + soma_pares;
@@ -44,16 +44,16 @@ int main() {
 
 
     if (num_de_termos_duplicados % 2 != 0){
-           
+
         soma_impares = (( 2.0*c + parte_baixo) / (parte_cima-c*6.0));
         resposta += soma_impares;
 
-        }
+    }
 
-    
-    
 
-    printf("a resposta e %f \n", resposta);
+
+
+    printf("\nA resposta e %f \n", resposta);
 
     return 0;
 
