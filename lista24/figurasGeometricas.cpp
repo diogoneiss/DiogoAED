3121d52dd97611faae510d5997c1d745f65d4ed6
diogo <diogo.oliveiran@gmail.com>
2019-06-26 11:15:36 -0300

    Iniciada lista 24

---
+++ lista24/figurasGeometricas.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <exception>
+#include "classes.h"
+
+
+// lidar com erros
+
+class Erro : public _exception {
+
+public:
+    Erro(){
+
+    }
+
+   //const char* what(){}
+};
+
+
+// prototipos
+void submenuQuadrados();
+void submenuTriangulos();
+void submenuCirculos();
+
+//fim prototipos
+
+// setar as variaveis estáticas de quantidade
+int FigGeometrica::quantidade = 0;
+int Quadrado::quantidade = 0;
+int Triangulo::quantidade = 0;
+int Circulo::quantidade = 0;
+
+int main(){
+
+
+int opcao;
+
+do{
+    try{
+        std::cout << "\nNo momento ha " << FigGeometrica::getQuantidade() << " figuras geometricas."<< std::endl;
+        std::cout << "Insira o numero correspondente ao submenu que quer acessar" << std::endl;
+
+        std::cout << "[0] sair do programa" << std::endl;
+        std::cout << "[1] circulo" << std::endl;
+        std::cout << "[2] quadrado" << std::endl;
+        std::cout << "[3] triangulo" << std::endl;
+        std::cout << "Insira o numero correspondente ao submenu que quer acessar: ";
+
+        std::cin >> opcao;
+
+        switch (opcao)
+            {
+            case 0:
+                break;
+                
+            case 1: submenuCirculos();
+                break;
+
+            case 2: submenuQuadrados();
+                break;
+
+            case 3: submenuTriangulos();        
+                break;
+            
+            default: throw(0);
+                break;
+            }
+        }
+        
+    catch(int n){
+        std::cout << "Erro na execucao, repetindo." << std::endl;
+    }
+
+}while(opcao != 0);
+
+std::cout << "Foram criadas " << FigGeometrica::getQuantidade() << " figuras geometricas ao longo do programa."<< std::endl;
+std::cout << "Fim do programa." << std::endl;
+
+}
+
+void submenuCirculos(){
+   
+    int raio;
+
+    Circulo *ptrCirculo;
+
+    std::cout << "Insira 0 caso queira voltar ao menu " << std::endl;
+
+    std::cout << "Insira o raio do Circulo a ser criado: ";
+    std::cin >> raio;
+
+    ptrCirculo = new Circulo(raio);
+
+    std::cout << "Perimetro: " << ptrCirculo->perimetro() << std::endl;
+    std::cout << "Area: " << ptrCirculo->area() << std::endl;
+    std::cout << "Num de Circulos: " << ptrCirculo->getQuantidade() << std::endl;
+    // destruir o ptr
+    delete ptrCirculo;
+}
+
+void submenuTriangulos(){
+   
+    
+    int ladoA;
+    int ladoB;
+    int ladoC;
+
+    Triangulo *ptrTriangulo;
+
+    
+    std::cout << "Insira 0 caso queira voltar ao menu " << std::endl;
+
+    // atrobuição de lados
+
+    std::cout << "Insira o lado A do triangulo a ser criado: ";
+    std::cin >> ladoA;
+
+    std::cout << "Insira o lado B do triangulo a ser criado: ";
+    std::cin >> ladoB;
+
+    std::cout << "Insira o lado C do triangulo a ser criado: ";
+    std::cin >> ladoC;
+
+    
+    ptrTriangulo = new Triangulo(ladoA, ladoB, ladoC);
+
+    std::string validadeTriangulo = ptrTriangulo->printarValidade();
+
+    std::cout << "Perimetro: " << ptrTriangulo->perimetro() << std::endl;
+    std::cout << "Area: " << ptrTriangulo->area() << std::endl;
+    std::cout << "Validade: " << validadeTriangulo  << std::endl;
+    std::cout << "Num de triangulos: " << ptrTriangulo->getQuantidade() << std::endl;
+    
+    // destruir o ptr
+    delete ptrTriangulo;
+}
+
+void submenuQuadrados(){
+       
+    int lado;
+
+    std::cout << "Insira 0 caso queira voltar ao menu " << std::endl;
+
+    std::cout << "Insira o lado do quadrado a ser criado: ";
+    std::cin >> lado;
+
+    Quadrado *ptrQuadrado;
+    ptrQuadrado = new Quadrado(lado);
+
+    std::cout << "Perimetro: " << ptrQuadrado->perimetro() << std::endl;
+    std::cout << "Area: " << ptrQuadrado->area() << std::endl;
+        std::cout << "Num de quadrados: " << ptrQuadrado->getQuantidade() << std::endl;
+    
+    // destruir o ptr
+    delete ptrQuadrado;
+}
